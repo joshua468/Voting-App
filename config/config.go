b7d273880d5b9c84d3b5ded087e8d65d8779a11c
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-11-18 20:48:26 +0100

    config.go

--- config/config.go
+++ config/config.go
@@ -1,58 +1,23 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres" 
-	"gorm.io/gorm"            
 )
 
-var DB *gorm.DB
 func LoadEnv() {
-	// Load environment variables from the .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 }
 
-// GetDBConfig returns the database connection string
-func GetDBConfig() string {
-	// Read database configuration from environment variables
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	// Return the PostgreSQL connection string
-	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port)
-}
-
-
-func InitializeDatabase() {
-	dsn := GetDBConfig()
-
-	// Connect to the PostgreSQL database using GORM
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
-	}
-
-	// Log successful connection
-	log.Println("Database connected successfully")
-}
-
-// GetJWTSecretKey returns the JWT secret key for signing and parsing tokens
-func GetJWTSecretKey() string {
+func GetJWTSecret() string {
 	secret := os.Getenv("JWT_SECRET")
 	if secret == "" {
-		log.Fatal("JWT_SECRET is required in the .env file")
+		log.Fatal("JWT_SECRET not set in .env")
 	}
 	return secret
 }
