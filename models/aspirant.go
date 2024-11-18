7884953dbf4888c8d823ba50de623a695d12d860
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-11-18 20:52:30 +0100

    aspirant.go

--- models/aspirant.go
+++ models/aspirant.go
@@ -6,9 +6,7 @@ import (
 
 type Aspirant struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Party    string `json:"party"`
-	Photo    string `json:"photo"`
-	EctionID uint   `json:"election_id"`
-	Votes    int    `gorm:default:0  json:"votes"`
+	Name       string `json:"name" gorm:"not null"`
+	ElectionID uint   `json:"election_id" gorm:"not null"`
+	Votes      int    `json:"votes" gorm:"default:0"`
 }
