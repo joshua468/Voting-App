9cd72702eb368c269a85ecd5c002f8374141fb30
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-11-19 14:19:32 +0100

    main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,20 +4,28 @@ import (
 	"log"
 	"os"
 
-	"github.com/joshua468/voting-app/config"
-	"github.com/joshua468/voting-app/controllers"
-	"github.com/joshua468/voting-app/database"
-	"github.com/joshua468/voting-app/middlewares"
-
 	"github.com/gin-gonic/gin"
+	"github.com/joshua468/voting-app/internal/config"
+	"github.com/joshua468/voting-app/internal/controllers"
+	"github.com/joshua468/voting-app/internal/database"
+	"github.com/joshua468/voting-app/internal/models" // Import your models package
+
+	"github.com/joshua468/voting-app/internal/middlewares"
 )
 
 func main() {
+
 	// Load environment variables
 	config.LoadEnv()
 
-	// Initialize database
+	// Initialize the database connection
 	database.InitDB()
+
+	// Run AutoMigrate to ensure the database schema matches the models
+	err := database.DB.AutoMigrate(&models.User{}, &models.Election{}, &models.Aspirant{}, &models.Vote{}) // Add all models here
+	if err != nil {
+		log.Fatal("Error running migrations:", err)
+	}
 
 	// Initialize Gin router
 	r := gin.Default()
