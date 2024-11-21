783c6f8cc3a166a82a7d59ae05d4549f3232470c
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-11-21 14:39:28 +0100

    election.go

--- internal/models/election.go
+++ internal/models/election.go
@@ -1,11 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Election struct {
 	gorm.Model
-	Title     string     `json:"title" gorm:"not null"`
-	StartDate string     `json:"start_date" gorm:"not null"`
-	EndDate   string     `json:"end_date" gorm:"not null"`
-	Aspirants []Aspirant `json:"aspirants" gorm:"foreignKey:ElectionID"`
+	Title     string     `json:"title"`
+	Status    string     `json:"status"`
+	WinnerID  uint       `json:"winner_id"`
+	Aspirants []Aspirant `gorm:"foreignKey:ElectionID" json:"aspirants"`
 }
